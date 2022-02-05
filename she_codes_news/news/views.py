83dd1985a064427a5ad0d8489721c686e816d732
Kristie <kmlbrandis@gmail.com>
2022-02-05 12:52:09 +0800

    updated author in default news data and started trying to add success messages to forms

--- she_codes_news/news/views.py
+++ she_codes_news/news/views.py
@@ -1,7 +1,11 @@
 from django.views import generic
+from django.contrib.messages.views import SuccessMessageMixin
 from django.urls import reverse_lazy
 from .models import NewsStory
 from .forms import StoryForm
+from django.http import HttpResponse
+from django.contrib import messages
+
 
 class IndexView(generic.ListView):
     template_name = 'news/index.html'
@@ -21,8 +25,39 @@ class StoryView(generic.DetailView):
     template_name = 'news/story.html'
     context_object_name = 'story'
 
-class AddStoryView(generic.CreateView):
+# class AddStoryView(generic.CreateView):
+#     form_class = StoryForm
+#     context_object_name = 'storyForm'
+#     template_name = 'news/createStory.html'
+#     # success_url = reverse_lazy('news:index')
+#     success_message = 'Your story has been published'
+#     def form_valid(self, form):
+#         form.instance.author = self.request.user
+#         return super().form_valid(form)
+
+
+# stuffing around with success messages 
+
+
+class SuccessMessageMixin:
+    success_message = 'Your story has been published'
+
+    def form_valid(self,form):
+        response = super().form_valid(form)
+        success_message = self.get_success_message(form.cleaned_data)
+        if success_message:
+            messages.success(self.request, success_message)
+        return response
+    
+    def get_success_message(self, cleaned_data):
+        return self.success_message % cleaned_data
+
+class AddStoryView(SuccessMessageMixin, CreateView):
     form_class = StoryForm
     context_object_name = 'storyForm'
     template_name = 'news/createStory.html'
-    success_url = reverse_lazy('news:index')
+    # success_url = reverse_lazy('news:index')
+    success_message = 'Your story has been published'
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
