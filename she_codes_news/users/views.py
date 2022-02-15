0315386d4c8edf59c57cfb44ee8b4fb9c8d5d155
Kristie <kmlbrandis@gmail.com>
2022-02-15 17:46:45 +0800

    updated profile view

--- she_codes_news/users/views.py
+++ she_codes_news/users/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
-from django.views.generic import CreateView
-from django.views.generic import UpdateView
+from django.views import generic 
+
 from .forms import  CustomUserCreationForm
 from .forms import CustomSignUpForm
 from django.views import View
@@ -24,11 +24,15 @@ from .models import Profile
 #     template_name = 'users/login.html'
 
 
-class RegisterView(CreateView):
+class RegisterView(generic.CreateView):
     form_class = CustomSignUpForm
     # context_object_name = 'signup'
     success_url = reverse_lazy('users:login')
     template_name = 'users/signup.html'
+
+    # def form_valid(self, form):
+    #     r=super().form_valid(form)
+
 
 # class ProfileView(CreateView):
 #     template_name = 'users/profile.html'
@@ -59,33 +63,37 @@ class RegisterView(CreateView):
 #     return HttpResponse('successfully uploaded')
 
 # @method_decorator(login_required(login_url='login'), name='dispatch')
-class ProfileView(CreateView):
+class ProfileView(generic.UpdateView):
     form_class = ProfileForm
     template_name = 'users/profile.html'
+
+    def  get_object(self, queryset=None):
+        profile, created =Profile.objects.get_or_create(user=self.request.user)
+        return profile
     # @login_required
     # def get(self, request):
     #     return render(request,'users/profile.html')
     # profile = None
 
-    def dispatch(self, request, *args, **kwargs):
-        self.profile, __ = Profile.objects.get_or_create(user=request.user)
-        return super(ProfileView, self).dispatch(request, *args, **kwargs)
+    # def dispatch(self, request, *args, **kwargs):
+    #     self.profile, __ = Profile.objects.get_or_create(user=request.user)
+    #     return super(ProfileView, self).dispatch(request, *args, **kwargs)
 
     # def get(self, request):
     #     context = {'profile': self.profile, 'segment': 'profile'}
     #     return render(request, 'users/profile.html', context)
 
-    def post(self, request):
-        form = ProfileForm(request.POST, request.FILES, instance=self.profile)
+    # def post(self, request):
+    #     form = ProfileForm(request.POST, request.FILES, instance=self.profile)
 
-        if form.is_valid():
-            profile = form.save()
-            profile.user.first_name = form.cleaned_data.get('first_name')
-            profile.user.last_name = form.cleaned_data.get('last_name')
-            profile.user.email = form.cleaned_data.get('email')
-            profile.user.save()
+    #     if form.is_valid():
+    #         profile = form.save()
+    #         profile.user.first_name = form.cleaned_data.get('first_name')
+    #         profile.user.last_name = form.cleaned_data.get('last_name')
+    #         profile.user.email = form.cleaned_data.get('email')
+    #         profile.user.save()
 
-            messages.success(request, 'Profile saved successfully')
-        else:
-            messages.error(request, form_validation_error(form))
-        return redirect('users:profile')
+    #         messages.success(request, 'Profile saved successfully')
+    #     else:
+    #         messages.error(request, form_validation_error(form))
+    #     return redirect('users:profile')
